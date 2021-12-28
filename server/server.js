e4674cacf117610c2d6f52e4d305e2a3c9127132
laBruja <labrujasiete@gmail.com>
2021-12-27 16:12:27 -0800

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,31 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(publicPath));
-app.listen(port, ()=>{
+let server = http.createServer(app);
+let io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('new user connected');
+
+    socket.on('disconnect', ()=>{
+        console.log("user was disconected")
+    });
+})
+
+
+
+
+
+
+
+server.listen(port, ()=>{
     console.log(`server running port ${port}`);
 });
 
